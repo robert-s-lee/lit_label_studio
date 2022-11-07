3cfc50c5ff503b7c2f8c2a695835d8ea797cdf10
danbider <danbider@gmail.com>
2022-11-07 17:52:28 -0500

    MAX_CONNECT_ATTEMPTS added for start_project

--- lit_label_studio/build_labeling_task.py
+++ lit_label_studio/build_labeling_task.py
@@ -9,22 +9,16 @@ parser.add_argument('--label_studio_url', type=str)
 parser.add_argument('--data_dir', type=str)
 parser.add_argument('--api_key', type=str)
 parser.add_argument('--project_name', type=str)
-# TODO: add text file with label config
 parser.add_argument('--label_config', type=str)
 
 # TODO: remove capital letters
 args = parser.parse_args()
-LABEL_STUDIO_URL = args.label_studio_url
-PATH = args.data_dir
-API_KEY = args.api_key
-PROJECT_NAME = args.project_name
 
-# TODO: read label config text file into str
+# read label config text file into str
 with open(args.label_config, 'r') as f:
     label_config = f.read()
 
-print("label config type" , type(label_config))
-
+MAX_CONNECT_ATTEMPTS = 30
 # # Define the URL where Label Studio is accessible and the API key for your user account
 # LABEL_STUDIO_URL = 'http://localhost:8080' # that's gonna be different on the cloud and via lightning. in this script will have to use the internal ip property so that another work accesses it. 
 # # LABEL_STUDIO_URL = self.internal_ip
@@ -32,22 +26,23 @@ print("label config type" , type(label_config))
 # PATH = "/Users/danbiderman/Dropbox/Columbia/1.Dan/lit_label_studio/images_to_label" # TODO: generalize
 # API_KEY = '4949affb1e0883c20552b123a7aded4e6c76760b' # look at sectrets in lightning docs lightning --cloud has secrets key.
 # when someone launches lightning, we create for them API key for AWS, with a bucket-name that they write into. when a user starts, they have a video locally. we need them to give us the video, and we can send it to a designated bucket.
-basedir = os.path.basename(PATH)
+basedir = os.path.basename(args.data_dir)
 
 # Import the SDK and the client module
 
-
-# have a while loop here that tries to connect for up to 100 secs. it breaks when it is connected or if a 100 secods passed
 connected = False
+attempts = 0
 while not connected:
     try:
-        label_studio_client = Client(url=LABEL_STUDIO_URL, api_key=API_KEY)
-        connection = label_studio_client.check_connection()
+        label_studio_client = Client(url=args.label_studio_url, api_key=args.api_key)
         connected = True
-        print("Connected")
+        print("We are connected to LabelStudio URL: {} ".format(args.label_studio_url))
     except:
-        print("Not connected, sleeping")
+        print("Not connected to LabelStudio URL, retrying in one second...")
+        attempts += 1
         time.sleep(1)
+        if attempts > MAX_CONNECT_ATTEMPTS:
+            raise Exception("Could not connect to LabelStudio URL: {} after {} attempts".format(args.label_studio_url, MAX_CONNECT_ATTEMPTS))
 
 # connected = False
 # while connected == False:
@@ -62,24 +57,38 @@ while not connected:
 #     print("we are up")
 # else:
 #     print("we are not connected")
+# string_that_works = '''
+#         <!--Basic keypoint image labeling configuration for multiple regions-->
+#             <View>
+#             <KeyPointLabels name="kp-1" toName="img-1" strokeWidth="3">
+#                 <Label value="Bros" background="red" />
+#                 <Label value="Dan" background="blue" />
+#                 <Label value="Matt" background="green" />
+#             </KeyPointLabels>
+#             <Image name="img-1" value="$img" />
+#             </View>
+#             '''
 #### start project
-label_studio_project = label_studio_client.start_project(
-    title=PROJECT_NAME,
-    label_config='''
-    <!--Basic keypoint image labeling configuration for multiple regions-->
-    <View>
-    <KeyPointLabels name="kp-1" toName="img-1">
-        <Label value="Bros" background="red" />
-        <Label value="Dan" background="blue" />
-        <Label value="Matt" background="green" />
-    </KeyPointLabels>
-    <Image name="img-1" value="$img" />
-    </View>
-    '''
-)
+print("Creating LabelStudio project...")
+# try to create project
+project_created = False
+attempts = 0
+while not project_created:
+    try:
+        label_studio_project = label_studio_client.start_project(
+            title=args.project_name,
+            label_config=label_config)
+        project_created = True
+        print("LabelStudio Project created!")
+    except:
+        print("Project not created, retrying in one second...")
+        attempts += 1
+        time.sleep(1)
+        if attempts > MAX_CONNECT_ATTEMPTS:
+            raise Exception("Could not create LabelStudio project after {} attempts".format(MAX_CONNECT_ATTEMPTS))
 
 json = {
-"path": PATH,
+"path": args.data_dir,
 # "regex_filter": ".*png",
 # "use_blob_urls": "true",
 # "title": "string",
@@ -89,12 +98,16 @@ json = {
 "project": label_studio_project.id
 }
 
+print("Creating LabelStudio data source...")
 label_studio_project.make_request('POST', '/api/storages/localfiles', json=json)
+print("LabelStudio data source created.")
 
-
+print("Importing tasks...")
 img_list = []
 label_studio_prefix = f"data/local-files?d={basedir}/"
 # loop over the png files in the directory and add them as dicts to the lisr, using labelstudio path format
-image_list = [{"img": label_studio_prefix + f} for f in os.listdir(PATH) if f.endswith('.png')]
+image_list = [{"img": label_studio_prefix + f} for f in os.listdir(args.data_dir) if f.endswith('.png')]
 label_studio_project.import_tasks(image_list)
+print("%i Tasks imported." % len(image_list))
+
 
